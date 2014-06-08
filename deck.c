2cf767b707ebd33422f0ab47a15a31563959f15d
Peter-P22 <nimrod21@abv.bg>
2014-06-08 12:22:00 +0300

    Update deck.c

--- deck.c
+++ deck.c
@@ -4,22 +4,22 @@ int push_card(struct card_t card,struct deck_t *deck){
     if(deck->size < DECK_MAX){
         deck->cards[deck->size++] = card;
         return 0;
-    }
-    return -1;
+    } else
+        return -1;
 }
 
 int draw_card(struct deck_t *deck,struct card_t *card){
     if(deck->size > 0){
         card = &deck->cards[--deck->size];
         return 0;
-    }
-    return -1;
+    } else
+        return -1;
 }
 
 int look_card(struct deck_t *deck,struct card_t *card){
     if(deck->size > 0){
         card = &deck->cards[deck->size - 1];
         return 0;
-    }
-    return -1;
+    } else
+        return -1;
 }
