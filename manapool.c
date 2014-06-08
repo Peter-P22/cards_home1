99bc776cfe2e783a4aac4191b4b062c96c8fd706
Peter-P22 <nimrod21@abv.bg>
2014-06-08 14:46:10 +0300

    Update manapool.c

--- manapool.c
+++ manapool.c
@@ -2,11 +2,21 @@
 
 int can_put_card(struct card_t card_to_put, struct manapool_t pool){
 
-if(card_to_put.mana <= pool.mana)
-return 1;
-return 0;
+if(card_to_put.mana <= pool.mana){
+	return 1;
+}else
+	return 0;
 }
 
-int changing_mana(struct manapool_t *manapool,int remove) {
+int change_mana(struct manapool_t *manapool,int remove) {
 	manapool-> mana += remove;
+	if(manapool->mana > 10){
+		manapool->mana =10;
+	}if(manapool->mana < 0){
+		manapool->mana = 0;
+	}
+}
+int use_card(struct card_t card, struct manapool_t *pool){
+	pool->mana=pool->mana - card.mana;
+	
 }
